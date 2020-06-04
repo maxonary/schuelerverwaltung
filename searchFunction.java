850fb2b47a73c794224c83c96617a6a33787ee69
Ben Löken <ger55501@benloeken.de>
2020-06-04 10:48:37 +0200

    Methode für eine Suchfunktion aus unserem Projekt. Mit JTextField. Es muss nur noch gemäß meiner Kommentare bearbeitet werden.

---
+++ searchFunction.java
@@ -0,0 +1,45 @@
+import javax.swing.event.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Color;
+
+public class searchFunction
+{
+
+    public searchFunction()
+    {
+        //TableRowSorter sorter = new TableRowSorter<>("tableModel";     //"tableModel" mit dem Namen des Tabellen Model ersetzen
+        //"table".setRowSorter(sorter);                                  //"table" mit dem Namen der Tabelle ersetzen
+        
+        JTextField searchField = new JTextField("Suchen", 50);
+        searchField.setVisible(true);
+        //topPanel.add(searchField);                                      //"topPanel" muss mit dem Namen eines Panels ersetzt werden, Ich würde eins ganz oben über der Tabelle erstellen
+        searchField.setBackground(Color.decode("#777777"));
+        searchField.setForeground(Color.WHITE);
+        searchField.setHorizontalAlignment(JTextField.CENTER);
+          
+        searchField.getDocument().addDocumentListener(new DocumentListener() {
+        @Override
+        public void insertUpdate(DocumentEvent e) {
+                   search(searchField.getText());
+        }
+        @Override
+        public void removeUpdate(DocumentEvent e) {
+                  search(searchField.getText());
+        }
+        @Override
+        public void changedUpdate(DocumentEvent e) {
+                        search(searchField.getText());
+        }
+        public void search(String str) {
+        if (str.length() == 0) {
+                           //sorter.setRowFilter(null);                             //Das Kommentar muss noch entfernt werden, damit die Suchfunktion funktioniert
+        } else {
+                           //sorter.setRowFilter(RowFilter.regexFilter(str));       //Das Kommentar muss noch entfernt werden, damit die Suchfunktion funktioniert
+        }
+        }
+        });
+    }
+
+}
